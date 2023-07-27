6b75cdd1f72b05421e5e6143e9c4ee76d421231d
dycat <8856528+dycat@users.noreply.github.com>
2023-07-27 20:28:23 +0800

    ui: add basic ui

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,15 @@
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <p>Hello world</p>
-    </div>
-  </header>
-
   <main>
-    
+    <h2>RMB Convertor</h2>
+    <ul>
+      <input type="text">
+      <button>转换</button>
+    </ul>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
