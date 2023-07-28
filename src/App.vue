25938eedca784bd89e8abd95ba59f9ec3e2037b2
dycat <8856528+dycat@users.noreply.github.com>
2023-07-28 21:13:57 +0800

    ui: clean presets from vue

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@
 </script>
 
 <template>
-  <main>
-    <img src="@/assets/Logo.png" alt="Logo" width="150" height="150">
+  <main class="container mx-auto">
+    <div class="grid">
+      <img class="" src="@/assets/Logo.png" alt="Logo" width="150" height="150">
     <ul>
-      <input type="text">
+      <input type="text" class="mr-6">
       <button>转换</button>
     </ul>
+    </div>
+    
   </main>
 </template>
 
