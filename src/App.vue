2557efdba9555eb42eb59a4906ee84312fe81f92
dycat <8856528+dycat@users.noreply.github.com>
2023-07-28 21:34:51 +0800

    feat: link convertor function to ui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup>
+import { onMounted, ref, watch } from "vue";
+import { toFormal } from "./convertor";
 
+const num_input = ref('') 
+let formal_form = ref('')
+
+watch(num_input, async (num_input) => {
+  formal_form = toFormal(num_input);
+})
 </script>
 
 <template>
@@ -7,8 +15,9 @@
     <div class="grid">
       <img class="" src="@/assets/Logo.png" alt="Logo" width="150" height="150">
     <ul>
-      <input type="text" class="mr-6">
+      <input type="text" class="mr-6" v-model="num_input">
       <button>转换</button>
+      <p>{{ formal_form }}</p>
     </ul>
     </div>
     
